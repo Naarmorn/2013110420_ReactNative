91994ec33a5b09d803a28e350b66a527727ab1ff
Naarmorn <su.naarmorn_st@tni.ac.th>
2023-06-24 11:13:47 +0700

    "JavaScript"

---
+++ javascriptFile/access_property.js
@@ -0,0 +1,11 @@
+let user = {
+    id: 1,
+    name: "Naarmorn"
+};
+
+console.log("ID: ", user.id);
+console.log("Name: ", user.name);
+
+user.name = "Naarmorn";
+
+console.log("Name: ", user.name);
