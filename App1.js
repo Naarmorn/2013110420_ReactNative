cd62f70491b400a3a0171784809bd614aa9e48f0
Naarmorn <su.naarmorn_st@tni.ac.th>
2023-07-29 16:39:35 +0700

    Second Commit [Week5-July-29-2023]

---
+++ App1.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from 'react-native';
+import  ViewBoxesWithColorAndText from './components/ViewBoxesWithColorAndText';
+import DisplayandImage from './components/DisplayandImage';
+import LotsOfGreeting from './components/LotsOfGreeting';
+import Customtext from './components/Customtext';
+import Counter from './components/Counter';
+import Myinput from './components/Myinput';
+import IncrementCounter from './components/IncrementCounter';
+import Form from './components/Form';
+import Login from './components/Login';
+import DefineStyle from './components/DefineStyle';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/*<ViewBoxesWithColorAndText/>*/}
+      {/*<DisplayandImage/>*/}
+      {/*<LotsOfGreeting/>*/}
+      {/*<Customtext/>*/}
+      {/* <Counter/> */}
+      {/* <Myinput/> */}
+      {/* <IncrementCounter/> */}
+      {/* <Form/> */}
+      {/* <Login/> */}
+      <DefineStyle/>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+     flex:1,   
+     justifyContent:'center',    
+     alignItems:'center'   
+  }
+ })
+
+
---
+++ components/DefineStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from 'react-native'
+import React from 'react'
+import styles from '../components/styles'
+
+const DefineStyle = () => {
+  return (
+    <View style = {styles.container}>
+      <Text style = {[styles.title, styles.warning]}>Hello World</Text>
+    </View>
+  )
+}
+
+export default DefineStyle
