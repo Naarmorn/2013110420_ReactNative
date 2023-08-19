7fa79dcff60faf3e2af229cb7b914676459112b9
Naarmorn <su.naarmorn_st@tni.ac.th>
2023-08-19 12:13:04 +0700

    Second Commit [Lecture7-August-19-2023]

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import React from 'react'
 import Example_useEffect from './components/Example_useEffect'
 import UseEffectFlatList from './components/UseEffectFlatList'
 import RandomUsersScreen from './components/RandomUsersScreen'
-
+import FlatList_Example1 from './components/FlatList_Example1'
+import FlatList_HeaderFooter from './components/FlatList_HeaderFooter'
 const App = () => {
   return (
     <View>
       {/* <Example_useEffect/> */}
       {/* <UseEffectFlatList/> */}
-      <RandomUsersScreen/>
+      {/* <RandomUsersScreen/> */}
+      {/* <FlatList_Example1/> */}
+      <FlatList_HeaderFooter/>
     </View>
   )
 }
