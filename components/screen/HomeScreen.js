716b4b3f5a2ee85453df437c5d7698aad40f54b3
Naarmorn <su.naarmorn_st@tni.ac.th>
2023-09-02 10:22:55 +0700

    First Commit [Lecture9-September-02-2023]-2

--- components/screen/HomeScreen.js
+++ components/screen/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text,Button } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
 
@@ -6,7 +6,7 @@ const HomeScreen = ({navigation}) => {
   return (
     <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
       <Text>Home Screen</Text>
-      <Button title='Go to Details' onPress={()=>navigation.navigaye('Details')}/>
+      <Button title='Go to Details' onPress={()=>navigation.navigate('Details')}/>
     </View>
   )
 }
